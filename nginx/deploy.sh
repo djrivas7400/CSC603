ace242ca07c2cbe52b2bb92acbfb37ba41a8a452
djrivas7400 <85266216+djrivas7400@users.noreply.github.com>
2022-11-29 14:28:42 -0500

    Add files via upload

--- nginx/deploy.sh
+++ nginx/deploy.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+set -x
 
+cp /local/repository/nginx/nginx.conf .
+sed -i "s/MYDOMAIN/$(hostname -f)/g" nginx.conf
+
+sudo apt-get update && sudo apt-get install -y nginx
+sudo cp -f nginx.conf /etc/nginx/nginx.conf
+sudo systemctl restart nginx
