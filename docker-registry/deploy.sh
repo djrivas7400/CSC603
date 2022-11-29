fef3546192e033e70916c1e04bc8d7e550b6a9e9
djrivas7400 <85266216+djrivas7400@users.noreply.github.com>
2022-11-29 14:27:25 -0500

    Add files via upload

--- docker-registry/deploy.sh
+++ docker-registry/deploy.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+set -x
 
+docker run --entrypoint htpasswd registry:2.7.0 -Bbn $DOCKER_USER $DOCKER_PASSWORD > ./htpasswd
+
+helm repo add twuni https://helm.twun.io
+helm repo update
+helm install --namespace docker-registry --create-namespace --version 2.2.2 -f /local/repository/docker-registry/docker-registry-values.yml --set secrets.htpasswd=$(cat htpasswd) docker-registry twuni/docker-registry
+
+cp /local/repository/docker-registry/docker-registry-ingress.yml .
+sed -i "s/MYDOMAIN/$(hostname -f)/g" docker-registry-ingress.yml
+kubectl apply -f docker-registry-ingress.yml
+
+export DOCKER_REGISTRY=$(kubectl get ingress -n docker-registry -o jsonpath='{.items..metadata.annotations.docker-url}')
