d1bca5a444a7748ff413071cb9c055973ed9a2c6
djrivas7400 <85266216+djrivas7400@users.noreply.github.com>
2022-11-29 14:30:03 -0500

    Add files via upload

--- traefik/deploy.sh
+++ traefik/deploy.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+set -x
 
+helm repo add traefik https://helm.traefik.io/traefik
+helm repo update
+helm install --namespace traefik --create-namespace --version 20.5.2 -f /local/repository/traefik/traefik-values.yml traefik traefik/traefik
